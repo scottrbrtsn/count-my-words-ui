978cefdc5ee641886fd4bc3c05cdd7424f871dbe
scottrbtrsn <scott.peter.robertson@gmail.com>
2020-03-14 21:31:38 -0500

    Updates

---
+++ precache-manifest.24163e322d559e0c93773a49f3fa7a70.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e085e6a21d76b4e614624694ef19d7bf",
+    "url": "/count-my-words-ui/index.html"
+  },
+  {
+    "revision": "c56ac9bf62fa00ffc550",
+    "url": "/count-my-words-ui/static/css/main.6f6e6596.chunk.css"
+  },
+  {
+    "revision": "9a8e399e4d349bd50459",
+    "url": "/count-my-words-ui/static/js/2.2eafbb43.chunk.js"
+  },
+  {
+    "revision": "933f139c1acbc3546cc823b3421e06f7",
+    "url": "/count-my-words-ui/static/js/2.2eafbb43.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c56ac9bf62fa00ffc550",
+    "url": "/count-my-words-ui/static/js/main.b2661e97.chunk.js"
+  },
+  {
+    "revision": "aeddff232fef5ff5afe2",
+    "url": "/count-my-words-ui/static/js/runtime-main.91985b06.js"
+  }
+]);
